832858bcc0dba4f3a58d2162657cab68e63401cc
The Chaos <keios.is.awake@night.org>
2013-07-31 00:08:01 +0200

    multiple fixes; frontend directory listing works now

--- ftw_mirror/index.pl
+++ ftw_mirror/index.pl
@@ -24,6 +24,7 @@ use strict;
 use warnings;
 
 use CGI;
+use URI::Encode;
 use HTML::Template;
 use utf8;
 use CGI::Carp qw( fatalsToBrowser warningsToBrowser );
@@ -41,9 +42,16 @@ my $s_showdiskfree  = 0;
 
 # leave these alone
 my $m_errstring = '';
+my $m_baselink = '';
+my $m_fsize = '';
+my $m_filesize = '';
+my $m_permalink = '';
 
 my $m_cgi = CGI->new;
-my $m_template = HTML::Template->new( filename => "index.template", associate => + $m_cgi );
+my $m_uri = URI::Encode->new( encode_reserved => 0 );
+my $m_template = HTML::Template->new( filename => "index.template",
+                                      loop_context_vars => 1,
+                                      associate => + $m_cgi );
 
 #
 # functions
@@ -63,11 +71,23 @@ sub show_free_diskspace {
 # TODO
 }
 
+sub format_filesize {
+    my $size = shift;
+    my $exp = 0;
+    state $units = [qw( B KB MB GB )];
+    for ( @$units ) {
+        last if $size < 1024;
+        $size /= 1024;
+        $exp++;
+    }
+    return wantarray ? ( $size, $units->[$exp] ) : sprintf( "%.2f %s", $size, $units->[$exp] );
+}
+
 sub check_data_dir {
 # return values: 0 = not empty, 1 = empty, -1 = not existent
   my ( $dir ) = @_;
   my $file;
-  opendir my $dfh, $dir or die "Could not open dir: $!";
+  opendir my $dfh, $dir or die "Could not open dir: $!\n";
   if ( $dfh ){
       while ( defined( $file = readdir $dfh ) ){
           next if $file eq '.' or $file eq '..';
@@ -84,20 +104,41 @@ sub check_data_dir {
 #
 # code
 #
-
+print $m_cgi->header;
 process_error_messages(s_errormsg);
 
 if ( check_data_dir(s_datadir) ){
-    my @m_filelist <s_datadir/*>;
+    opendir my $m_dfh, s_datadir or die "Could not open dir: $!\n";
+    my @m_filelist = readdir $m_dfh;
+    sort @m_filelist;
+    my @m_loop;
+
+    $m_baselink = '/srv/www/te st/ftw_mirror/data';
+    $m_baselink =~ s/$ENV{DOCUMENT_ROOT}/$ENV{HTTP_HOST}\//;
 
     foreach my $m_file (@m_filelist) {
-        print $m_cgi->p($m_file),
-              $m_cgi->br();
+        next if $m_file eq '.' or $m_file eq '..';
+
+        $m_fsize = ( -s s_datadir ."/". $m_file);
+        $m_filesize = format_filesize($m_fsize);
+
+        $m_permalink = $m_uri->encode($m_baselink);
+        $m_permalink = "http://" . $m_permalink;
+
+        my %m_line = (
+            t_itemname => $m_file,
+            t_itemsize => $m_filesize,
+            t_itemlink => $m_cgi->a( { -href => "$m_permalink",
+                                       -title => "$m_file", },
+                                        "http://"."$m_baselink"."/"."$m_file" ),
+        );
+        push(@m_loop, \%m_line);
     }
+    $m_template->param(t_dirlisting => \@m_loop);
 } elsif ( check_data_dir(s_datadir) eq -1 ){
-    die "Could not open data directory: $!";
+    die "Could not open data directory: $!\n";
 } else {
-    print $m_cgi->p("data dir is empty!");
+    $m_template->param( 't_empty' => 'true' );
 }
 
 $m_template->param(
@@ -107,5 +148,4 @@ $m_template->param(
     t_subheader => s_subheader,
 );
 
-print $m_cgi->header;
 print $m_template->output;
